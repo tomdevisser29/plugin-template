e007a441e683ff2e24045f85ea426df92583dc72
Tom de Visser <tomdevisser29@gmail.com>
2025-03-21 16:18:35 +0100

    Add initial plugin file with basic metadata and directory constants

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: [plugin_name]
+ * Author: Tom de Visser
+ */
+
+define( '[PLUGIN_NAME]_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( '[PLUGIN_NAME]_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
