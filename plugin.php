6b05026bc2caa32de445da928c1eb8b44a63a168
Tom de Visser <tomdevisser29@gmail.com>
2025-03-21 16:46:36 +0100

    Update the placeholders to avoid errors

--- plugin.php
+++ plugin.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * Plugin Name: [Plugin_Name]
+ * Plugin Name: Plugin_Name
  * Author: Tom de Visser
- * Text Domain: [plugin-name]
+ * Text Domain: plugin-name
+ * Version: 0.1.0
  */
 
-define( '[PLUGIN_NAME]_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( '[PLUGIN_NAME]_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'PLUGIN_NAME_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_NAME_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-function [plugin_name]_init() {
+function plugin_name_init() {
   return '';
 }
