866ca91d08e0b8cec8026c0ae09129e5a61108d8
Tom de Visser <tomdevisser29@gmail.com>
2025-03-21 16:40:48 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
- * Plugin Name: [plugin_name]
+ * Plugin Name: [Plugin_Name]
  * Author: Tom de Visser
  */
 
 define( '[PLUGIN_NAME]_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( '[PLUGIN_NAME]_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+function [plugin_name]_init() {
+  return '';
+}
